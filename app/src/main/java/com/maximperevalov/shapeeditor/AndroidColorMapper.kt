7c9836d751a466decdb43bca9f6742cb72520719
a-mountain <frostnext2@gmail.com>
2020-10-21 22:26:37 +0300

    Add AndroidColorMapper.kt

--- app/src/main/java/com/maximperevalov/shapeeditor/AndroidColorMapper.kt
+++ app/src/main/java/com/maximperevalov/shapeeditor/AndroidColorMapper.kt
@@ -5,15 +5,20 @@ import com.maximperevalov.shapeeditor.domain.Color
 /**
  * Трансформує, кросплатформенний клас Сolor в реалізацію цього класу на Android
  */
-fun Color.toAndroidColor() = when (this) {
-    Color.RED -> android.graphics.Color.RED
-    Color.BLACK -> android.graphics.Color.BLACK
-    Color.WHITE -> android.graphics.Color.WHITE
-    Color.YELLOW -> android.graphics.Color.YELLOW
-    Color.BLUE -> android.graphics.Color.BLUE
-    Color.ORANGE -> android.graphics.Color.rgb(255, 128, 0)
-    Color.GRAY -> android.graphics.Color.GRAY
-    Color.GREEN -> android.graphics.Color.GREEN
-    Color.PURPLE -> android.graphics.Color.rgb(128, 0, 128)
-    Color.PINK -> android.graphics.Color.rgb(255, 192, 203)
+object AndroidColorMapper {
+
+    fun getAndroidColor(color: Color) = when (color) {
+        Color.RED -> android.graphics.Color.RED
+        Color.BLACK -> android.graphics.Color.BLACK
+        Color.WHITE -> android.graphics.Color.WHITE
+        Color.YELLOW -> android.graphics.Color.YELLOW
+        Color.BLUE -> android.graphics.Color.BLUE
+        Color.ORANGE -> android.graphics.Color.rgb(255, 128, 0)
+        Color.GRAY -> android.graphics.Color.GRAY
+        Color.GREEN -> android.graphics.Color.GREEN
+        Color.PURPLE -> android.graphics.Color.rgb(128, 0, 128)
+        Color.PINK -> android.graphics.Color.rgb(255, 192, 203)
+    }
 }
+
+fun Color.toAndroidColor() = AndroidColorMapper.getAndroidColor(this)
